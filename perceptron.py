0e42fcc36cd094d6f5ec4e0d0d2dd20156fa3500
Arthur Lapraye <arthurlapraye@yahoo.fr>
2015-01-10 15:56:57 +0100

    I believe in space

---
+++ perceptron.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python 
+
+
+def argmax(f, Y):
+	return max(Y,key=f)
+	
+#for epoch in range(1, K):
+#	for 
+
+#traits= set()
+
+#Vu que les vecteurs de traits sont binaires, c'est plus logique de les représenter par des set() que par des dicts
+def score(traits,poidsY):
+	score=0;
+	for t in traits:
+		score += poidsY[t]
+	
+	return score
+
+
+def classify(poids, traits):
+	
+	
+
